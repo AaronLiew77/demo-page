5563aaf31d82d0cb96ac073499dd76095e5034e9
Aaron Liew <103647952+AaronLiew77@users.noreply.github.com>
2025-10-13 20:03:53 +0800

    mida us without anti flicker

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
     <html lang="en">
      <head>
       {/* Mida Account */}
-      <MidaScript projectKey="E3jxwZ6ldLqbzYg90mMX8O" useAntiFlicker={true}/>
+      <MidaScript projectKey="E3jxwZ6ldLqbzYg90mMX8O"/>
 
       {/* US */}
       {/* <script type="text/javascript" async src="https://cdn.mida.so/js/optimize.js?key=bJVGmqv2Yv4QDBlWgj9k3o"></script>      EU */}
