4eff5ac9b42d0f96ec9ecf1ac2ae589787457cc0
Aaron Liew <103647952+AaronLiew77@users.noreply.github.com>
2025-10-28 17:45:32 +0800

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,10 +27,31 @@ export default function RootLayout({
     <html lang="en">
     <head>
       <Script 
+        id="flicker-prevention"
+        strategy="beforeInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            var timeout = 3000; // Timeout value to remove the flicker (in milliseconds)
+            !function(h,i,d,e){var t,n=h.createElement("style");n.id=e,n.innerHTML="body{opacity:0}",h.head.appendChild(n),t=d,i.rmfk=function(){var t=h.getElementById(e);t&&t.parentNode.removeChild(t)},setTimeout(i.rmfk,t)}(document,window,timeout,"abhide");
+          `
+        }}
+      />
+      <Script 
         src="https://cdn.mida.so/js/optimize.js?key=E3jxwZ6ldLqbzYg90mMX8O" 
         strategy="beforeInteractive"
       />
-      <link rel="preconnect" href="https://api-us.mida.so"></link>
+      <Script 
+        id="preconnect-mida"
+        strategy="beforeInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            const link = document.createElement('link');
+            link.rel = 'preconnect';
+            link.href = 'https://api-us.mida.so';
+            document.head.appendChild(link);
+          `
+        }}
+      />
     </head>
  
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
