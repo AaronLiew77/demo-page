0d8fdcc59d83bd2266791f537a8daa38da87fb7a
Aaron Liew <103647952+AaronLiew77@users.noreply.github.com>
2025-12-05 14:36:03 +0800

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,10 +38,10 @@ export default function RootLayout({
           `
         }}
       />
-      <Script 
+      {/* <Script 
         src="https://cdn.mida.so/js/optimize.js?key=E3jxwZ6ldLqbzYg90mMX8O" 
         strategy="beforeInteractive"
-      />
+      /> */}
       {/* <Script 
         id="preconnect-mida"
         strategy="beforeInteractive"
