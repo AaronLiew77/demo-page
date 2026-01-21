94aa02c46fa825c98a03d72549d577d3e7abbdb3
Aaron Liew <103647952+AaronLiew77@users.noreply.github.com>
2026-01-21 23:38:38 +0800

    fix: new flicker

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,26 +11,17 @@ export default function RootLayout({
   return (
     <html lang="en">
     <head>
-      {/* <MidaScript projectKey="E3jxwZ6ldLqbzYg90mMX8O" useAntiFlicker={true} antiFlickerTimeout={3000}/> */}
-
-    
-      <Script dangerouslySetInnerHTML={{
-        __html: `
-          !function(){var e="body {opacity: 0 !important;}",t=document.createElement("style");t.type="text/css",t.id="page-hide-style",t.styleSheet?t.styleSheet.cssText=e:t.appendChild(document.createTextNode(e)),document.head.appendChild(t),window.rmo=function(){var e=document.getElementById("page-hide-style");e&&(e.parentNode.removeChild(e),document.body.style.opacity="")},setTimeout(window.rmo,3e3)}();
-        `
-      }} />
-
-
-      {/* <Script 
-        id="flicker-prevention"
+      <Script
+        id="anti-flicker"
         strategy="beforeInteractive"
         dangerouslySetInnerHTML={{
           __html: `
-            var timeout = 1000; // Timeout value to remove the flicker (in milliseconds)
-            !function(h,i,d,e){var t,n=h.createElement("style");n.id=e,n.innerHTML="body{opacity:0}",h.head.appendChild(n),t=d,i.rmfk=function(){var t=h.getElementById(e);t&&t.parentNode.removeChild(t)},setTimeout(i.rmfk,t)}(document,window,timeout,"abhide");
+            setTimeout(() => {
+              document.body.style.display = '';
+            }, 3000);
           `
         }}
-      /> */}
+      />
       <Script 
         src="https://cdn.mida.so/js/optimize.js?key=E3jxwZ6ldLqbzYg90mMX8O" 
         strategy="beforeInteractive"
@@ -50,7 +41,7 @@ export default function RootLayout({
   
     </head>
  
-      <body>
+      <body style={{ display: 'none' }}>
         {children}
       </body>
     </html>
