19155e18858abd0766855954c31febc800e59308
Aaron Liew <103647952+AaronLiew77@users.noreply.github.com>
2026-01-21 17:43:32 +0800

    new script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,24 +1,7 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Script from "next/script";
 import { MidaScript } from "mida-nextjs";
 
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-export const metadata: Metadata = {
-  title: "SaaSify - Build Your SaaS Faster Than Ever",
-  description: "The all-in-one platform that helps you launch, scale, and grow your SaaS business with powerful tools and analytics.",
-};
 
 export default function RootLayout({
   children,
@@ -29,7 +12,11 @@ export default function RootLayout({
     <html lang="en">
     <head>
       {/* <MidaScript projectKey="E3jxwZ6ldLqbzYg90mMX8O" useAntiFlicker={true} antiFlickerTimeout={3000}/> */}
-      {/* <Script 
+
+
+
+
+      <Script 
         id="flicker-prevention"
         strategy="beforeInteractive"
         dangerouslySetInnerHTML={{
@@ -38,12 +25,12 @@ export default function RootLayout({
             !function(h,i,d,e){var t,n=h.createElement("style");n.id=e,n.innerHTML="body{opacity:0}",h.head.appendChild(n),t=d,i.rmfk=function(){var t=h.getElementById(e);t&&t.parentNode.removeChild(t)},setTimeout(i.rmfk,t)}(document,window,timeout,"abhide");
           `
         }}
-      /> */}
+      />
       <Script 
         src="https://cdn.mida.so/js/optimize.js?key=E3jxwZ6ldLqbzYg90mMX8O" 
         strategy="beforeInteractive"
       />
-      {/* <Script 
+      <Script 
         id="preconnect-mida"
         strategy="beforeInteractive"
         dangerouslySetInnerHTML={{
@@ -54,11 +41,11 @@ export default function RootLayout({
             document.head.appendChild(link);
           `
         }}
-      /> */}
+      />
   
     </head>
  
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <body>
         {children}
       </body>
     </html>
