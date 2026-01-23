cd549331f852829bae3a8401f45f245daea98809
Aaron Liew <103647952+AaronLiew77@users.noreply.github.com>
2026-01-23 20:36:57 +0800

    try fix posthog again

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,9 @@
 import "./globals.css";
 import Script from "next/script";
 import { MidaScript } from "mida-nextjs";
-
+import { PostHogProvider } from "./providers/PostHogProvider";
+import { PostHogPageView } from "./components/PostHogPageView";
+import { Suspense } from "react";
 
 export default function RootLayout({
   children,
@@ -55,7 +57,12 @@ export default function RootLayout({
     </head>
  
       <body style={{ opacity: 0 }}>
-        {children}
+        <PostHogProvider>
+          <Suspense fallback={null}>
+            <PostHogPageView />
+          </Suspense>
+          {children}
+        </PostHogProvider>
       </body>
     </html>
   )
